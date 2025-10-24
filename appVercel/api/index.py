25f3288b4f24a831ffa8f1a3bc182719a473fea6
Alejandro <89873076+Saenz-77@users.noreply.github.com>
2025-10-24 00:42:36 -0500

    Refactor notificar_asesor to use env variable for token
    Updated the notificar_asesor function to use environment variable for authorization token.

--- appVercel/api/index.py
+++ appVercel/api/index.py
@@ -1,33 +1,35 @@
-# index.py
+# api/index.py
+
 from flask import Flask, request, jsonify
 import requests
 import json
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home():
     return "🟢 Servidor Flask en Vercel activo."
 
 @app.route("/notificar-asesor", methods=["POST"])
 def notificar_asesor():
-    data = request.json
+    data = request.get_json()
     number = data.get("number")
     text = data.get("text")
 
     url = "https://graph.facebook.com/v22.0/834560259747367/messages"
     headers = {
-        'Authorization': 'Bearer EAFd59J08Mk8BPZCIXU90KLK43VFHZB0jn4oh6pZClCfRlXsWcRNCNZBJANWEpZAZBDhrvyo81FZAIkfkxvSZB5YQJNsp4kiZAlems9b6Iy6GUbZCLZAQN4SPuDFojr2JzS1mxNZCzSdYiYo8ci2YFzb5vZAMatOmmY3yXbhud3qWkpX2p57TpXNrJK6mMjFH3juVBWSVlnwZDZD',
-        'Content-Type': 'application/json'
+        "Authorization": f"Bearer {os.environ.get('WHATSAPP_TOKEN')}",
+        "Content-Type": "application/json"
     }
+
     payload = {
         "messaging_product": "whatsapp",
         "to": number,
         "type": "text",
-        "text": {"body": text}
+        "text": { "body": text }
     }
 
     print("📤 Enviando a WhatsApp:", payload)
     response = requests.post(url, headers=headers, data=json.dumps(payload))
     return jsonify(response.json())
-
